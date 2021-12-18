2497524146130eaaa76244eb91d9ee36ad664035
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2021-12-18 20:49:19 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/pjappsandgames/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pjappsandgames/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.pjappsandgames.workshopmongo.domain.Post;
 import com.pjappsandgames.workshopmongo.domain.User;
+import com.pjappsandgames.workshopmongo.dto.AuthorDTO;
 import com.pjappsandgames.workshopmongo.reposity.PostReposity;
 import com.pjappsandgames.workshopmongo.reposity.UserReposity;
 
@@ -30,16 +31,17 @@ public class Instantiation implements CommandLineRunner{
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userReposity.deleteAll();
+		postReposity.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-	
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);             
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bomd dia", "Acordei feliz hoje!", maria);
 		
 		userReposity.saveAll(Arrays.asList(alex, maria, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));             
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bomd dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postReposity.saveAll(Arrays.asList(post1, post2));
 	}
 
