81d725a3764dd746358a4ccaeb14d3b6b229f909
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2021-12-19 13:01:48 -0300

    Consulta simples com @Query

--- src/main/java/com/pjappsandgames/workshopmongo/reposity/PostReposity.java
+++ src/main/java/com/pjappsandgames/workshopmongo/reposity/PostReposity.java
@@ -3,6 +3,7 @@ package com.pjappsandgames.workshopmongo.reposity;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pjappsandgames.workshopmongo.domain.Post;
@@ -10,6 +11,8 @@ import com.pjappsandgames.workshopmongo.domain.Post;
 @Repository
 public interface PostReposity extends MongoRepository<Post, String>{
 	
+	@Query("{ 'tile': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
