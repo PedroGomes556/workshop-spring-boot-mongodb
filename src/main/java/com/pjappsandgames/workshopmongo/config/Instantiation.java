4d7501a14a11115c95501fab8521c8873ed5a7fc
PedroGomes556 <pedrogomesps4xboxone@gmail.com>
2021-12-19 12:32:40 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/pjappsandgames/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pjappsandgames/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.pjappsandgames.workshopmongo.domain.Post;
 import com.pjappsandgames.workshopmongo.domain.User;
 import com.pjappsandgames.workshopmongo.dto.AuthorDTO;
+import com.pjappsandgames.workshopmongo.dto.CommentDTO;
 import com.pjappsandgames.workshopmongo.reposity.PostReposity;
 import com.pjappsandgames.workshopmongo.reposity.UserReposity;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));             
 		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bomd dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));	
 		
 		postReposity.saveAll(Arrays.asList(post1, post2));
 		
